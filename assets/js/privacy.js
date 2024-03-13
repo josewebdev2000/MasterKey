6ce1567130bfb93f2e75752bc86bfba3e87b045c
josewebdev2000 <josewebdev@proton.me>
2024-03-13 16:01:34 -0400

    Added Smooth Animation Effect to Hash Links

--- assets/js/privacy.js
+++ assets/js/privacy.js
@@ -3,6 +3,29 @@
 function mainContact()
 {
     console.log("This is the privacy page");
+    smoothlyScrollLinkHash();
+}
+
+function smoothlyScrollLinkHash()
+{
+    $("a").on('click', function(event) {
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } 
+    });
 }
 
 $(document).ready(mainContact);
