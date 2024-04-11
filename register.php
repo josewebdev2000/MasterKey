38ea396657c2ce25753f606fa8e55f8c302277a5
josewebdev2000 <josewebdev@proton.me>
2024-04-11 15:35:06 -0400

    Began to build front-end of register and login pages

--- register.php
+++ register.php
@@ -1,3 +1,24 @@
 <?php require_once "templates/header.php"; ?>
+<?php require_once "templates/main-header.php"; ?>
+
+<main class="container-fluid bg-img-container bg-golden-key-img bg-img-light-orange-hue flex-065">
+    <div class="row">
+        <div class="col-12">
+            <h2 class="text-center mt-3 mb-4">Register</h2>
+        </div>
+    </div>
+
+    <div class="row">
+        <!--Introductory Section-->
+        <div class="col-12 mb-4 d-flex justify-content-center">
+            <section class="text-center p-3 hero rborder-25 dark-peach-hero-2">
+                <p class="lead fsize-150">Create a new <em><b>MasterKey</b></em> account</p>
+                <p class="lead fsize-150">Start securing your credentials with us</p>
+                <p class="lead fsize-150">You won't have to worry about remembering passwords anymore</p>
+            </section>
+        </div>
+    </div>
+</main>
+
 <?php require_once "templates/main-footer.php"; ?>
 <?php require_once "templates/footer.php";?>
